b2af055be7ce5da9b414d15f48d8e879e983684e
Muhammad Ubaid Raza <mubaidr@gmail.com>
2024-03-29 12:30:07 +0500

    enable tailwind dark mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  colorMode: {
+    preference: "dark",
+    fallback: "dark",
+    dataValue: "theme",
+    classSuffix: "",
+  },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  tailwindcss: {
+    editorSupport: true,
+    viewer: true,
+  },
 })
