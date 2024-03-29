ed0032e80b98ec2571313d7bee9e944255a7a060
Muhammad Ubaid Raza <mubaidr@gmail.com>
2024-03-29 13:14:58 +0500

    [wip] course stats

--- components/CourseStats.vue
+++ components/CourseStats.vue
@@ -1,7 +1,43 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>Course Stats</div>
+  <section class="my-32">
+    <span class="text-lg tracking-widest font-semibold leading-loose">
+      YOUR #1 SOURCE OF VUE.JS COURSES
+    </span>
+    <div class="flex gap-12">
+      <div class="flex-1">
+        <h2 class="text-6xl font-bold">
+          Full Stack
+          <span>Vue Training</span>
+          Solutions
+        </h2>
+      </div>
+      <div class="flex-1 flex text-center justify-evenly">
+        <div>
+          <div class="text-6xl font-bold">763</div>
+          <div>
+            <img src="/assets/video.svg" class="h-4" />
+            <span>Video Lessons</span>
+          </div>
+        </div>
+        <div>
+          <div class="text-6xl font-bold">40</div>
+          <div>
+            <img src="/assets/course.svg" class="h-4" />
+            <span>Courses</span>
+          </div>
+        </div>
+        <div>
+          <div class="text-6xl font-bold">64</div>
+          <div>
+            <img src="/assets/time.svg" class="h-4" />
+            <span>15 Hours</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style lang="scss" scoped></style>
